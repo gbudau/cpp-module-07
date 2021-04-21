5e550d71f7c4d2b46f13ee0a1ee42b877df3f231
gbudau <54190400+gbudau@users.noreply.github.com>
2021-04-21 19:25:26 +0200

    ex00 Refactor tests
    Added templates for testing swap(), min() and max() function templates

--- ex00/whatever.cpp
+++ ex00/whatever.cpp
@@ -3,8 +3,7 @@
 
 template <typename T>
 void	swap(T & a, T & b) {
-	T	temp(a);
-
+	T temp = a;
 	a = b;
 	b = temp;
 }
@@ -19,7 +18,54 @@ const T&	max(const T& x, const T& y) {
 	return (x > y) ? x : y;
 }
 
+template <typename T>
+void	test_swap(T & a, T & b) {
+	std::cout << "##### Testing swap() #####\n";
+	std::cout << "a = " << a << "\n";
+	std::cout << "b = " << b << "\n";
+	std::cout << "Swapping values\n";
+	::swap(a, b);
+	std::cout << "a = " << a << "\n";
+	std::cout << "b = " << b << "\n";
+}
+
+template <typename T>
+void	test_min(const T & a, const T & b) {
+	std::cout << "##### Testing min() #####\n";
+	std::cout << "a = " << a << "\n";
+	std::cout << "b = " << b << "\n";
+	std::cout << "min(a, b) = " << ::min(a, b) << "\n";
+}
+
+template <typename T>
+void	test_max(const T & a, const T & b) {
+	std::cout << "##### Testing max() #####\n";
+	std::cout << "a = " << a << "\n";
+	std::cout << "b = " << b << "\n";
+	std::cout << "max(a, b) = " << ::max(a, b) << "\n";
+}
+
+template <typename T>
+void	test_all(T & a, T & b) {
+	test_swap(a, b);
+	test_min(a, b);
+	test_max(a, b);
+}
+
 int	main() {
+	{
+		std::cout << "##### Testing <int> type #####\n";
+		int	a = 0;
+		int	b = 42;
+		test_all(a, b);
+	}
+	{
+		std::cout << "\n##### Testing <std::string> type #####\n";
+		std::string a = "hello";
+		std::string b = "world";
+		test_all(a, b);
+	}
+	std::cout << "\n##### Tests from the subject #####\n";
 	{
 		int a = 2;
 		int b = 3;
@@ -36,85 +82,5 @@ int	main() {
 		std::cout << "c = " << c << ", d = " << d << std::endl;
 		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-	}
-	{
-		int	n1 = 0;
-		int	n2 = 42;
-		int	n3 = 42;
-
-		std::cout << "\n##### Test swap() #####\n";
-		std::cout << "n1 is: " << n1 << "\n";
-		std::cout << "n2 is: " << n2 << "\n";
-		std::cout << "Swapping values\n";
-		::swap(n1, n2);
-		std::cout << "n1 is: " << n1 << "\n";
-		std::cout << "n2 is: " << n2 << "\n";
-
-		std::cout << "\n";
-
-		std::string	s1 = "hello";
-		std::string	s2 = "world";
-		std::string s3 = "world";
-
-		std::cout << "s1 is: " << s1 << "\n";
-		std::cout << "s2 is: " << s2 << "\n";
-		std::cout << "Swapping values\n";
-		::swap(s1, s2);
-		std::cout << "s1 is: " << s1 << "\n";
-		std::cout << "s2 is: " << s2 << "\n";
-
-		std::cout << "\n";
-
-		std::cout << "##### Test min() #####\n";
-		std::cout << "n1 is: " << n1 << "\n";
-		std::cout << "n2 is: " << n2 << "\n";
-		std::cout << "n3 is: " << n3 << "\n";
-		std::cout << "min(n1, n2) = " << ::min(n1, n2) << "\n";
-		std::cout << "min(n1, n3) = " << ::min(n1, n3) << "\n";
-		if (&::min(n1, n3) == &n1) {
-			std::cout << "min(n1, n3) return n1\n";
-		} else {
-			std::cout << "min(n1, n3) return n3\n";
-		}
-
-		std::cout << "\n";
-
-		std::cout << "s1 is: " << s1 << "\n";
-		std::cout << "s2 is: " << s2 << "\n";
-		std::cout << "s3 is: " << s3 << "\n";
-		std::cout << "min(s1, s2) = " << ::min(s1, s2) << "\n";
-		std::cout << "min(s1, s3) = " << ::min(s1, s3) << "\n";
-		if (&::min(s1, s3) == &s1) {
-			std::cout << "min(s1, s3) return s1\n";
-		} else {
-			std::cout << "min(s1, s3) return s3\n";
-		}
-
-		std::cout << "\n";
-
-		std::cout << "##### Test max() #####\n";
-		std::cout << "n1 is: " << n1 << "\n";
-		std::cout << "n2 is: " << n2 << "\n";
-		std::cout << "n3 is: " << n3 << "\n";
-		std::cout << "max(n1, n2) = " << ::max(n1, n2) << "\n";
-		std::cout << "max(n1, n3) = " << ::max(n1, n3) << "\n";
-		if (&::max(n1, n3) == &n1) {
-			std::cout << "max(n1, n3) return n1\n";
-		} else {
-			std::cout << "max(n1, n3) return n3\n";
-		}
-
-		std::cout << "\n";
-
-		std::cout << "s1 is: " << s1 << "\n";
-		std::cout << "s2 is: " << s2 << "\n";
-		std::cout << "s3 is: " << s3 << "\n";
-		std::cout << "max(s1, s2) = " << ::max(s1, s2) << "\n";
-		std::cout << "max(s1, s3) = " << ::max(s1, s3) << "\n";
-		if (&::max(s1, s3) == &s1) {
-			std::cout << "max(s1, s3) return s1\n";
-		} else {
-			std::cout << "max(s1, s3) return s3\n";
-		}
 	}
 }
